728db2c121c3eefff31fa9d4a2c2cb837709e21f
Safa Uğurlu <ugurlusafa579@gmail.com>
2024-04-28 02:32:01 +0300

    Entityler oluşturuldu

---
+++ Core/Seinfeld.Domain/Common/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeinfeldApi.Domain.Common
+{
+	public class EntityBase:IEntityBase
+	{
+		public int Id { get; set; }
+		public DateTime CreatedDate { get; set; } = DateTime.Now;//default olarak gelsin diye
+		public bool IsDeleted { get; set; }=false;//default olarak gelsin diye
+    }
+}
---
+++ Core/Seinfeld.Domain/Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿using SeinfeldApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeinfeldApi.Domain.Entities
+{
+	public class Brand :EntityBase//Entity base zaten Ientitybase den miras aldığı için yazmamıza gerek yok.
+	{
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
