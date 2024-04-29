5d422d7942d48522d29e80dc4a3bf414158241d7
Safa Uğurlu <ugurlusafa579@gmail.com>
2024-04-29 05:44:05 +0300

    ders8 yapıldı

--- Seinfeld.Application/InterFaces/Repositories/IWriteRepository.cs
+++ Seinfeld.Application/InterFaces/Repositories/IWriteRepository.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SeinfeldApi.Application.InterFaces.Repositories
 {
-	public interface IWriteRepository
+	public interface IWriteRepository<T> where T : class
 	{
+		Task AddAsync(T entity);
+		Task AddRangeAsync(IList<T> entities);
+		Task<T> UpdateAsync(T entity);
+	
+		Task HardDeleteAsync(T entity);// çok çok nadir kullanırız
+		
+
 	}
 }
