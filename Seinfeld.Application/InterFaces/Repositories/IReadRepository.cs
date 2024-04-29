6390b90e61e943bcc46832b6cdab16a9623aa8bb
Safa Uğurlu <ugurlusafa579@gmail.com>
2024-04-29 03:17:37 +0300

    read repository yapıldı

---
+++ Seinfeld.Application/InterFaces/Repositories/IReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using SeinfeldApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace SeinfeldApi.Application.InterFaces.Repositories
+{
+	public interface IReadRepository<T> where T : class, IEntityBase , new()
+	{
+		Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null,
+			Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+			Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+			bool enableTracking = false);
+
+		//null geçilebilmesinin sebebi ısdeleted olanları istediğimiz entityde alabiliriz. datetime'ı belli aralıklar vs sorgular daha rahat ve kolay olur.
+
+
+		Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>>? predicate = null,
+			Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+			Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+			bool enableTracking = false, int currentPage = 1,int pageSize =3);//mevcut sayfada gördüğüm ilk 3 veriyi alcam
+
+		Task<T> GetAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, bool enableTracking = false);
+
+
+			
+
+		IQueryable<T> Find(Expression<Func<T, bool>> predicate, bool enableTracking = false); //üstte ki methodlar to list şeklinde olacağı için, list olarak görmek istemediklerimizi bunla yapıcaz
+
+		Task<int> CountAsync(Expression<Func<T, bool>>? predicate= null);
+	}
+}
