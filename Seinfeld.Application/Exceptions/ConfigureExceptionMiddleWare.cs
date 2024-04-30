6268c246d0f711921c34be6ba836f4a610aeb864
Safa Uğurlu <ugurlusafa579@gmail.com>
2024-05-01 01:59:12 +0300

    hata yönetimi yapıldı

---
+++ Seinfeld.Application/Exceptions/ConfigureExceptionMiddleWare.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeinfeldApi.Application.Exceptions
+{
+	public static class ConfigureExceptionMiddleWare
+	{
+		public static void ConfigureExceptionMiddleware(this IApplicationBuilder app)
+		{
+			app.UseMiddleware<ExcepctionMiddleware>();
+		}
+	}
+}
