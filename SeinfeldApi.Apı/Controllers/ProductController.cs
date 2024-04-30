c41ffe611a04e3b8f2907fad4bb485f67b68582a
Safa Uğurlu <ugurlusafa579@gmail.com>
2024-04-30 07:40:25 +0300

    request handler response yapıldı

---
+++ SeinfeldApi.Apı/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using System.Runtime.CompilerServices;
+using SeinfeldApi.Application.Features.Products.Queries.GetAllProducts;
+
+namespace SeinfeldApi.Apı.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		private readonly IMediator mediator;
+
+		public ProductController(IMediator mediator)
+		{
+			this.mediator = mediator;
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetAllProducts()
+		{
+			var response = await mediator.Send(new GetAllProductsQueryRequest());
+			return Ok(response);
+		}
+	}
+}
